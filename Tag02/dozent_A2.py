87ab7dfb35b91c8afb195bf3d8da77a46038c0b9
anna-s-dotcom <57437060+anna-s-dotcom@users.noreply.github.com>
2020-01-14 15:26:12 +0100

    Add files via upload

---
+++ Tag02/dozent_A2.py
@@ -0,0 +1,9 @@
+# Erstelle zwei funktionen. Eine, die zu einer Zahl 2 hinzu addiert und eine, die von einer Zahl 2 abzieht.
+#
+# Erstelle einen shared Value Datentyp mit dem Wert 100.
+#
+# Erstelle für jede Funktion je 20 Prozesse, die diese Funktion aufrufen.
+#
+# Gib den Endwert der shared Value Variable aus.
+#
+# Bonus: Teste Zeit mit Lock und Zeit ohne Lock.
